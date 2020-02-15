d27bb724a6aca46814e8e8f8b372bce0aad9f367
Anastasia Kalinina <stasya.kalinina@gmail.com>
2020-02-15 19:58:13 +0500

    created client for api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
-const getResourse = async (url) => {
-  let res = await fetch(url);
+class SwapiService {
+  _apiBase = 'https://swapi.co/api';
+
+  async getResourse(url) {
+    let res = await fetch(`${this._apiBase}${url}`);
+  if (!res.ok) {
+    throw new Error(`could not fetch ${url}, received ${res.status}`)
+  }
+
   let body = await res.json();
   return body;
-};
+  }
 
-getResourse('https://swapi.co/api/people/5/')
-  .then((body) => {
-    console.log(body);
-  });
+  async getAllPeople() {
+    let res = await this.getResourse(`/people/`);
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResourse(`/people/${id}`);
+  }
+
+  async getAllPlanets() {
+    let res = await this.getResourse(`/planets/`);
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResourse(`/planets/${id}`);
+  }
+
+  async getAllStarhips() {
+    let res = await this.getResourse(`/starships/`);
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResourse(`/starships/${id}`);
+  }
+}
+
+const swapi = new SwapiService();
+// swapi.getAllPeople().then((body) => {
+//   body.forEach((item) => {
+//     console.log(item.name);
+//   });
+// });
+
+swapi.getPerson(3).then((body) => {
+  console.log(body.name);
+});
