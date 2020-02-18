d2ef182a2f9bbe0db4be4efc1e8cb990c5b5a504
Anastasia Kalinina <stasya.kalinina@gmail.com>
2020-02-18 14:05:50 +0500

    added data from api

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,29 +1,62 @@
 import React, {Component} from 'react';
+import SwapiService from '../../services/swapi-service';
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
 
+  swapiService = new SwapiService();
+
+  state = {
+    id: null,
+    name: null,
+    population: null,
+    rotationPeriod: null,
+    diameter: null,
+  }
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  updatePlanet() {
+    let id = Math.floor(Math.random() * 25 + 2);
+    this.swapiService
+      .getPlanet(id)
+      .then((planet) => {
+        this.setState({
+          id,
+          name: planet.name,
+          population: planet.population,
+          rotationPeriod: planet.rotation_period,
+          diameter: planet.diameter,
+        })
+      })
+  }
+
   render() {
+    let { id, name, population, rotationPeriod, diameter } = this.state;
+
     return (
       <article className="random-planet jumbotron rounded">
         <img
-            src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
-            className="random-planet__img"
-            alt="planet"/>
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+          className="random-planet__img"
+          alt="planet"/>
         <div className="random-planet__content">
-          <h2>Planet Name:</h2>
+          <h2>{name}</h2>
           <ul className="random-planet__list">
             <li className="random-planet__item">
               <span className="term">Population:</span>
-              <span>123125</span>
+              <span>{population}</span>
             </li>
             <li className="random-planet__item">
               <span className="term">Rotation Period:</span>
-              <span>42</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className="random-planet__item">
               <span className="term">Diameter:</span>
-              <span>120</span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>
